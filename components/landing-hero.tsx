a44c435fe35b593c40b17ac6e626dac0b3a620f4
Cedric Lambert <wiselife.pro@gmail.com>
2024-04-04 01:34:21 -0400

    minor changes

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -13,7 +13,7 @@ export const LandingHero = () => {
         <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
             <h1>AI Tarot Tool</h1>
             {/* <h1>brought to you by</h1> */}
-            <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to bg-pink-600">
+            <div className="rainbow_gradient_1">
                 <TypeWriterComponent
                   options={({
                     strings:[
--- components/reading-mat.tsx
+++ components/reading-mat.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import  { ITarotCard } from "@/models/TarotCards";
+import Image from 'next/image';
 
 interface Props {
     cards: ITarotCard[];
@@ -14,7 +15,7 @@ const ReadingMat: React.FC<Props> = ({ cards, onCardRemove }) => {
         <div className="glassmorphism flex flex-row justify-center gap-4 mb-2">
             {cards.map((card, index) => (
                 <div key={index} className="flex flex-col items-center">
-                    <img src={card.cardImage} alt={card.title} className="w-full max-w-sm h-auto bg-transparent object-cover" />
+                    <Image src={card.cardImage} alt={card.title} className="w-full max-w-sm h-auto bg-transparent object-cover" />
                     <p className="text-center">{positions[index]}</p>
                     {/* <button onClick={() => onCardRemove(index)} className="mt-2 bg-red-500 text-white px-2 py-1 rounded">Remove</button> */}
                 </div>
